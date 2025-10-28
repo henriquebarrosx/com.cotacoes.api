233adbe87065f3c7af01ebfe42e96db78974e2a6
Henrique Barros <henriquebarrosx@gmail.com>
2025-10-28 01:36:44 -0300

    refactor: remove unused exported module

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import './infra/database/main';
-import { createDatabase } from './infra/database/database';
 import { createHttpServer } from "./infra/http_server/main";
 import { messageBroker } from "./infra/message_broker/main";
 import { forexProsWorkerRunner } from "./app/worker/forex_pros_ws/main";
 
 /* CORE SERVICES */
-export const database = createDatabase();
-
-export const server = createHttpServer();
-
+createHttpServer();
 await messageBroker.connect();
 
 /* WORKERS */
